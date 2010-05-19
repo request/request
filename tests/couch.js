1139efedb5aad4a328c1d8ff45fe77839a69169f
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-05-19 12:11:08 -0700

    Cleaning up tests.

--- tests/couch.js
+++ tests/couch.js
@@ -1,33 +1,30 @@
 var request = require('../lib/main'), 
-    sys = require('sys');
+    sys = require('sys'),
+    assert = require('assert');
 
 function testports (port) {
   var uri = port ? 'http://mikeal.couchone.com' + ":" + port : 'http://mikeal.couchone.com';
   sys.puts(uri)
   request({uri:uri}, function (error, response, body) {
     if (error) {throw new Error(error)};
-    if (!response.statusCode == 200) {
-      throw new Error("Wrong status code "+response.statusCode);
-    }
-    var v = '{"couchdb":"Welcome",'
-    if (body.slice(0, v.length) !== v) {
-      throw new Error("Unexpected body\n"+body);
-    }
+    assert.equal(response.statusCode, 200);
+    assert.equal(body.slice(0, '{"couchdb":"Welcome",'.length), '{"couchdb":"Welcome",');
   })
 }
 testports();
 testports(80)
 testports(5984)
 
-var randomnumber=Math.floor(Math.random()*11).toString();
+var randomnumber=Math.floor(Math.random()*100000000).toString();
 request({uri:'http://mikeal.couchone.com/testjs', method:'POST', body:'{"_id":"'+randomnumber+'"}'}, 
         function (error, response, body) {
           if (error) {throw new Error(error)};
-          if (!response.statusCode == 201) {
-            throw new Error("Wrong status code "+response.statusCode);
-          }
+          assert.equal(response.statusCode, 201);
         });
 
-request({uri:'http://gmail.com'}, function (error, response, body) {
-  sys.puts(error)
+var options = {uri:'http://gmail.com'};
+request(options, function (error, response, body) {
+  assert.equal(response.statusCode, 200);
+  assert.equal(options.uri.host, 'www.google.com');
+  assert.equal(response.socket.port, 443);
 })
