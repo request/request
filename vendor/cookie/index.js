055a7268b40425643d23bd6a4f09c7268dbab680
John Hurliman <jhurliman@jhurliman.org>
2011-11-29 02:04:33 -0800

    Applying a modified version of pull request #106 as suggested by janjongboom

--- vendor/cookie/index.js
+++ vendor/cookie/index.js
@@ -40,9 +40,7 @@ var Cookie = exports = module.exports = function Cookie(str, req) {
     : Infinity;
 
   // Default or trim path
-  this.path = this.path
-    ? this.path.trim()
-    : url.parse(req.url).pathname;
+  this.path = this.path || '/';
 };
 
 /**
