a482e4802e11fd122b12e18d1b18b49850fef823
Mikeal Rogers <mikeal.rogers@gmail.com>
2012-02-17 16:07:47 -0800

    Updating copyright for the new year.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Copyright 2010-2011 Mikeal Rogers
+// Copyright 2010-2012 Mikeal Rogers
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
