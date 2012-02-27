0ea2351ef017ada9b8472f8d73086715ebe30c6a
Mikeal Rogers <mikeal.rogers@gmail.com>
2012-02-26 17:03:22 -0800

    Fixes #190. outdated check on options.json from before we had boolean support.

--- main.js
+++ main.js
@@ -784,7 +784,10 @@ request.put = function (uri, options, callback) {
 request.head = function (uri, options, callback) {
   var params = initParams(uri, options, callback);
   params.options.method = 'HEAD'
-  if (params.options.body || params.options.requestBodyStream || params.options.json || params.options.multipart) {
+  if (params.options.body || 
+      params.options.requestBodyStream || 
+      (params.options.json && typeof params.options.json !== 'boolean') || 
+      params.options.multipart) {
     throw new Error("HTTP HEAD requests MUST NOT include a request body.")
   }
   return request(params.uri, params.options, params.callback)
