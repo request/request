337718baa08cafb3e706d275fd7344a3c92363bb
Mikeal Rogers <mikeal.rogers@gmail.com>
2013-03-01 11:44:56 -0800

    Smarter test runner.

--- tests/run.js
+++ tests/run.js
@@ -1,59 +1,40 @@
 var spawn = require('child_process').spawn
   , exitCode = 0
   , timeout = 10000
+  , fs = require('fs')
   ;
 
-var tests = [
-    'test-basic-auth.js'
-  , 'test-body.js'
-  , 'test-cookie.js'
-  , 'test-cookiejar.js'
-  , 'test-defaults.js'
-  , 'test-digest-auth.js'
-  , 'test-errors.js'
-  , 'test-form.js'
-  , 'test-follow-all-303.js'
-  , 'test-follow-all.js'
-  , 'test-headers.js'
-  , 'test-httpModule.js'
-  , 'test-https.js'
-  , 'test-https-strict.js'
-  , 'test-oauth.js'
-  , 'test-params.js'
-  , 'test-pipes.js'
-  , 'test-pool.js'
-  , 'test-protocol-changing-redirect.js'
-  , 'test-proxy.js'
-  , 'test-piped-redirect.js'
-  , 'test-qs.js'
-  , 'test-redirect.js'
-  , 'test-timeout.js'
-  , 'test-toJSON.js'
-  , 'test-tunnel.js'
-]
+fs.readdir(__dirname, function (e, files) {
+  if (e) throw e
 
-var next = function () {
-  if (tests.length === 0) process.exit(exitCode);
+  var tests = files.filter(function (f) {return f.slice(0, 'test-'.length) === 'test-'})
 
-  var file = tests.shift()
-  console.log(file)
-  var proc = spawn('node', [ 'tests/' + file ])
+  var next = function () {
+    if (tests.length === 0) process.exit(exitCode);
 
-  var killed = false
-  var t = setTimeout(function () {
-    proc.kill()
-    exitCode += 1
-    console.error(file + ' timeout')
-    killed = true
-  }, timeout)
+    var file = tests.shift()
+    console.log(file)
+    var proc = spawn('node', [ 'tests/' + file ])
 
-  proc.stdout.pipe(process.stdout)
-  proc.stderr.pipe(process.stderr)
-  proc.on('exit', function (code) {
-    if (code && !killed) console.error(file + ' failed')
-    exitCode += code || 0
-    clearTimeout(t)
-    next()
-  })
-}
-next()
+    var killed = false
+    var t = setTimeout(function () {
+      proc.kill()
+      exitCode += 1
+      console.error(file + ' timeout')
+      killed = true
+    }, timeout)
+
+    proc.stdout.pipe(process.stdout)
+    proc.stderr.pipe(process.stderr)
+    proc.on('exit', function (code) {
+      if (code && !killed) console.error(file + ' failed')
+      exitCode += code || 0
+      clearTimeout(t)
+      next()
+    })
+  }
+  next()
+    
+})
+
+
