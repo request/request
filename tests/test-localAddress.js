24175993f6c362f7fca5965feb0a11756f00baf3
Way, No <noway@2ch.hk>
2013-04-22 07:11:06 +1200

    Improving test-localAddress.js

--- tests/test-localAddress.js
+++ tests/test-localAddress.js
@@ -1,9 +1,15 @@
 var request = request = require('../index')
   , assert = require('assert')
   ;
- 
+
+request.get({
+  uri: 'http://www.google.com', localAddress: '1.2.3.4' // some invalid address
+}, function(err, res) {
+  assert(!res)
+})
+
 request.get({
   uri: 'http://www.google.com', localAddress: '127.0.0.1'
-}, function(err) {
-  assert.equal(err.code, 'ENETUNREACH')
+}, function(err, res) {
+  assert(!res)
 })
