32405d8e4f6c72069268c6069207445c6929aafb
Fred K. Schott <fschott@box.com>
2014-08-20 15:53:20 -0700

    toJSON no longer results in an infinite loop, returns simple objects

--- tests/test-toJSON.js
+++ tests/test-toJSON.js
@@ -7,8 +7,16 @@ var s = http.createServer(function (req, resp) {
   resp.statusCode = 200
   resp.end('asdf')
 }).listen(8080, function () {
-  var r = request('http://localhost:8080', function (e, resp) {
-    assert.equal(JSON.parse(JSON.stringify(r)).response.statusCode, 200)
+  var r = request({
+    url: 'http://localhost:8080',
+    headers: {foo: 'bar'}
+  }, function (e, resp) {
+    assert.equal(JSON.parse(JSON.stringify(r)).uri.href, r.uri.href)
+    assert.equal(JSON.parse(JSON.stringify(r)).method, r.method)
+    assert.equal(JSON.parse(JSON.stringify(r)).headers.foo, r.headers.foo)
+    assert.equal(JSON.parse(JSON.stringify(resp)).statusCode, resp.statusCode)
+    assert.equal(JSON.parse(JSON.stringify(resp)).body, resp.body)
+    assert.equal(JSON.parse(JSON.stringify(resp)).headers.date, resp.headers.date)
     s.close()
   })
 })
