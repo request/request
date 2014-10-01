8bac8c8c25672a0bf489a876c64a1208e0233814
FredKSchott <FredKSchott@gmail.com>
2014-10-01 08:06:41 -0700

    linting-fix: remove unused variables

--- request.js
+++ request.js
@@ -1,7 +1,6 @@
 var optional = require('./lib/optional')
   , http = require('http')
   , https = optional('https')
-  , tls = optional('tls')
   , url = require('url')
   , util = require('util')
   , stream = require('stream')
@@ -578,7 +577,6 @@ Request.prototype.init = function (options) {
     var full_path = self.uri.href.replace(self.uri.protocol+'/', '');
 
     var lookup = full_path.split('/');
-    var error_connecting = true;
 
     var lookup_table = {};
     do { lookup_table[lookup.join('/')]={} } while(lookup.pop())
