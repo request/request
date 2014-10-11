bd2a9180b150b56f47729f587259686528df5149
James Nylen <jnylen@gmail.com>
2014-10-11 13:18:22 -0500

    Try to diagnose Travis error better

--- tests/test-agentOptions.js
+++ tests/test-agentOptions.js
@@ -3,35 +3,36 @@ var request = require('../index')
   , server  = require('./server')
   , tape    = require('tape')
 
-var s
-  , port = 8111
+var s = server.createServer(function (req, resp) {
+  resp.statusCode = 200
+  resp.end('')
+})
 
 tape('setup', function(t) {
-  s = server.createServer(function (req, resp) {
-    resp.statusCode = 200
-    resp.end('')
-  }).listen(port, function() {
+  s.listen(s.port, function() {
     t.end()
   })
 })
 
 tape('without agentOptions should use global agent', function(t) {
-  t.plan(3)
   var r = request(s.url, function(err, res, body) {
+    if (err) console.log(err)
     t.equal(err, null)
     t.deepEqual(r.agent, http.globalAgent)
     t.equal(Object.keys(r.pool).length, 0)
+    t.end()
   })
 })
 
 tape('with agentOptions should apply to new agent in pool', function(t) {
-  t.plan(3)
   var r = request(s.url, {
     agentOptions: { foo: 'bar' }
   }, function(err, res, body) {
+    if (err) console.log(err)
     t.equal(err, null)
     t.equal(r.agent.options.foo, 'bar')
     t.equal(Object.keys(r.pool).length, 1)
+    t.end()
   })
 })
 
