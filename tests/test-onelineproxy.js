2bbedb7dac2f33918bec85363368a87eba8c6790
James Nylen <jnylen@gmail.com>
2014-10-11 00:33:56 -0500

    Rewrite tests/test-onelineproxy.js to use tape

--- tests/test-onelineproxy.js
+++ tests/test-onelineproxy.js
@@ -1,9 +1,9 @@
 var http = require('http')
   , assert = require('assert')
   , request = require('../index')
-  ;
+  , tape = require('tape')
 
-var server = http.createServer(function (req, resp) {
+var server = http.createServer(function(req, resp) {
   resp.statusCode = 200
   if (req.url === '/get') {
     assert.equal(req.method, 'GET')
@@ -14,10 +14,10 @@ var server = http.createServer(function (req, resp) {
   if (req.url === '/put') {
     var x = ''
     assert.equal(req.method, 'PUT')
-    req.on('data', function (chunk) {
+    req.on('data', function(chunk) {
       x += chunk
     })
-    req.on('end', function () {
+    req.on('end', function() {
       assert.equal(x, 'content')
       resp.write('success')
       resp.end()
@@ -26,21 +26,30 @@ var server = http.createServer(function (req, resp) {
   }
   if (req.url === '/proxy') {
     assert.equal(req.method, 'PUT')
-    return req.pipe(request('http://localhost:8080/put')).pipe(resp)
+    return req.pipe(request('http://localhost:6767/put')).pipe(resp)
   }
-
   if (req.url === '/test') {
-    return request('http://localhost:8080/get').pipe(request.put('http://localhost:8080/proxy')).pipe(resp)
+    return request('http://localhost:6767/get').pipe(request.put('http://localhost:6767/proxy')).pipe(resp)
   }
   throw new Error('Unknown url', req.url)
-}).listen(8080, function () {
-  request('http://localhost:8080/test', function (e, resp, body) {
-    if (e) throw e
-    if (resp.statusCode !== 200) throw new Error('statusCode not 200 ' + resp.statusCode)
-    assert.equal(body, 'success')
-    server.close()
+})
+
+tape('setup', function(t) {
+  server.listen(6767, function() {
+    t.end()
   })
 })
 
+tape('chained one-line proxying', function(t) {
+  request('http://localhost:6767/test', function(err, res, body) {
+    t.equal(err, null)
+    t.equal(res.statusCode, 200)
+    t.equal(body, 'success')
+    t.end()
+  })
+})
 
-
+tape('cleanup', function(t) {
+  server.close()
+  t.end()
+})
