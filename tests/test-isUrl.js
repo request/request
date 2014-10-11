ad7c485735531ef2ba0dec0de0b03f44574e3053
James Nylen <jnylen@gmail.com>
2014-10-11 00:07:57 -0500

    Expand tests for hostname and port passed as uri
    See #1138.

--- tests/test-isUrl.js
+++ tests/test-isUrl.js
@@ -51,7 +51,7 @@ tape('hostname and port', function(t) {
   })
 })
 
-tape('hostname and port', function(t) {
+tape('hostname and port 1', function(t) {
   request({
     uri: {
       protocol: 'http:',
@@ -61,6 +61,33 @@ tape('hostname and port', function(t) {
   }, function(err, res, body) {
     t.equal(err, null)
     t.equal(body, 'ok')
+    t.end()
+  })
+})
+
+tape('hostname and port 2', function(t) {
+  request({
+    protocol: 'http:',
+    hostname: 'localhost',
+    port: 6767
+  }, {
+    // need this empty options object, otherwise request thinks no uri was set
+  }, function(err, res, body) {
+    t.equal(err, null)
+    t.equal(body, 'ok')
+    t.end()
+  })
+})
+
+tape('hostname and port 3', function(t) {
+  request({
+    protocol: 'http:',
+    hostname: 'localhost',
+    port: 6767
+  }, function(err, res, body) {
+    t.notEqual(err, null)
+    t.equal(err.message, 'options.uri is a required argument')
+    t.equal(body, undefined)
     t.end()
   })
 })
