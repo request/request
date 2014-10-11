f514a7bd6ddc58e5937b0619b36438b83a9599a4
James Nylen <jnylen@gmail.com>
2014-10-11 00:42:55 -0500

    Rewrite tests/test-optional.js to use tape

--- tests/test-optional.js
+++ tests/test-optional.js
@@ -1,5 +1,9 @@
-var assert = require('assert')
-  , optional = require('../lib/optional')
-  , copy = optional('../lib/copy');
+var optional = require('../lib/optional')
+  , copy = optional('../lib/copy')
+  , tape = require('tape')
 
-assert.equal(module,module.children[1].parent);
+tape('optional modules show as being loaded by the module that requested them', function(t) {
+  t.equal(module.children[1].exports, copy)
+  t.equal(module, module.children[1].parent)
+  t.end()
+})
