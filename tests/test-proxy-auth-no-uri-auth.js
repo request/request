6b80cef74a5ffffa0dd8363af52da979a514b0de
James Nylen <jnylen@gmail.com>
2014-10-11 03:10:26 -0500

    Rewrite tests/test-proxy-auth-no-uri-auth.js to use tape

--- tests/test-proxy-auth-no-uri-auth.js
+++ tests/test-proxy-auth-no-uri-auth.js
@@ -1,41 +1,44 @@
 var server = require('./server')
   , events = require('events')
   , stream = require('stream')
-  , assert = require('assert')
   , fs = require('fs')
   , request = require('../index')
   , path = require('path')
   , util = require('util')
-  ;
+  , tape = require('tape')
 
-var port = 6768
-  , called = false
-  , proxiedHost = 'google.com'
-  ;
+var s = server.createServer()
 
-var s = server.createServer(port)
-s.listen(port, function () {
-  s.on('http://google.com/', function (req, res) {
-    called = true
-    assert.equal(req.headers.host, proxiedHost)
-    assert.equal(req.headers['proxy-authorization'], 'Basic dXNlcjpwYXNz')
-    console.error("headers", req.headers)
-    res.writeHeader(200)
-    res.end()
-  })
-  request ({
-    url: 'http://'+proxiedHost,
-    proxy: 'http://user:pass@localhost:'+port
-    /*
-    //should behave as if these arguments where passed:
-    url: 'http://localhost:'+port,
-    headers: {host: proxiedHost}
-    //*/
-  }, function (err, res, body) {
-    s.close()
+tape('setup', function(t) {
+  s.listen(s.port, function() {
+    t.end()
   })
 })
 
-process.on('exit', function () {
-  assert.ok(called, 'the request must be made to the proxy server')
+tape('proxy', function(t) {
+  var called = false
+
+  s.on('http://google.com/', function(req, res) {
+    called = true
+    t.equal(req.headers.host, 'google.com')
+    t.equal(req.headers['proxy-authorization'], 'Basic dXNlcjpwYXNz')
+    t.equal(req.headers.authorization, undefined)
+    res.writeHeader(200)
+    res.end()
+  })
+
+  request ({
+    url: 'http://google.com',
+    proxy: 'http://user:pass@localhost:' + s.port
+  }, function(err, res, body) {
+    t.equal(err, null)
+    t.equal(res.statusCode, 200)
+    t.equal(called, true, 'the request must be made to the proxy server')
+    t.end()
+  })
+})
+
+tape('cleanup', function(t) {
+  s.close()
+  t.end()
 })
