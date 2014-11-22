5eb86dcdaf10cae2af9a9aad7032c47586eae1b7
simov <simeonvelichkov@gmail.com>
2014-11-22 11:13:27 +0200

    Fix 'piping from a file' test

--- tests/test-pipes.js
+++ tests/test-pipes.js
@@ -137,6 +137,7 @@ tape('piping from a file', function(t) {
     if (req.method === 'PUT') {
       t.equal(req.headers['content-type'], 'application/javascript')
       t.end()
+      res.end()
     }
   })
   fs.createReadStream(__filename).pipe(request.put(s.url + '/pushjs'))
@@ -275,12 +276,5 @@ tape('request.pipefilter is called correctly', function(t) {
 
 tape('cleanup', function(t) {
   s.close()
-  // TODO - which test is causing the process not to exit?
-  setTimeout(function() {
-    t.end()
-    setTimeout(function() {
-      /*eslint no-process-exit:0*/
-      process.exit(0)
-    }, 10)
-  }, 300)
+  t.end()
 })
