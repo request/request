8e1b63db2134154cb949065d0bf1e3db9fd1e295
Eirik S. Morland <eirik@morland.no>
2014-12-08 17:25:56 +0100

    Add browser tests to npm test. Comment on eslint option.

--- tests/browser/test.js
+++ tests/browser/test.js
@@ -1,6 +1,9 @@
 'use strict'
 
 if (!Function.prototype.bind) {
+  // This is because of the fact that phantom.js does not have Function.bind.
+  // This is a bug in phantom.js.
+  // More info: https://github.com/ariya/phantomjs/issues/10522
   /*eslint no-extend-native:0*/
   Function.prototype.bind = require('function-bind')
 }
