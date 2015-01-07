d0737c88a1848f7b2bb7961cb1a45cd7e9a01466
Eirik S. Morland <eirik@morland.no>
2015-01-07 18:02:42 +0000

    Fix lint errors.

--- tests/browser/karma.conf.js
+++ tests/browser/karma.conf.js
@@ -30,5 +30,5 @@ module.exports = function(config) {
         ]
       }
     }
-  });
+  })
 }
--- tests/browser/start.js
+++ tests/browser/start.js
@@ -1,28 +1,34 @@
-var spawn = require('child_process').spawn;
-var https = require('https');
-var fs = require('fs');
+'use strict'
+var spawn = require('child_process').spawn
+var https = require('https')
+var fs = require('fs')
+var path = require('path')
 
-https.createServer({
-    key: fs.readFileSync(__dirname + '/ssl/server.key'),
-    cert: fs.readFileSync(__dirname + '/ssl/server.crt'),
-    ca: fs.readFileSync(__dirname + '/ssl/ca.crt'),
+var server = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, '/ssl/server.key')),
+    cert: fs.readFileSync(path.join(__dirname, '/ssl/server.crt')),
+    ca: fs.readFileSync(path.join(__dirname, '/ssl/ca.crt')),
     requestCert: true,
     rejectUnauthorized: false
   }, function (req, res) {
     // Set CORS header, since that is something we are testing.
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.writeHead(200);
-    res.end("Can you hear the sound of an enormous door slamming in the depths of hell?\n");
-}).listen(8000, function() {
-  console.log('Started https server for karma tests on port 8000');
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.writeHead(200)
+    res.end('Can you hear the sound of an enormous door slamming in the depths of hell?\n')
+})
+server.listen(8000, function() {
+  console.log('Started https server for karma tests on port 8000')
   // Spawn process for karma.
   var c = spawn('karma', [
     'start',
-    __dirname + '/karma.conf.js'
-  ]);
-  c.stdout.pipe(process.stdout);
+    path.join(__dirname, '/karma.conf.js')
+  ])
+  c.stdout.pipe(process.stdout)
   c.on('exit', function(c) {
     // Exit process with karma exit code.
-    process.exit(c);
-  });
-});
+    if (c !== 0) {
+      throw new Error('Karma exited with status code ' + c)
+    }
+    server.close()
+  })
+})
