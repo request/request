b4822ea912bea6d8f75404f468a34bba01871c50
Rowan Wookey <admin@rwky.net>
2015-01-08 19:46:39 +0000

    Added test for errors emitted by form-data

---
+++ tests/test-form-data-error.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var request = require('../index')
+  , tape = require('tape')
+
+function runTest(t) {
+  var req
+    , form
+    , reqOptions = {
+      url: 'http://localhost:8080/',
+    }
+
+    req = request.post(reqOptions, function (err, res, body) {
+      t.equal(err.message,'Arrays are not supported.')
+      t.end()
+    })
+
+    form = req.form()
+    form.append('field',['value1','value2'])
+}
+
+
+tape('re-emit formData errors', function(t) {
+  runTest(t)
+})
