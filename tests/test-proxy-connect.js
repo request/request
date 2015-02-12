b860c41fa6b698beca4f6c5fc7cb853c24dd4f97
Mikeal Rogers <mikeal.rogers@gmail.com>
2015-02-12 13:16:15 -0800

    Fixing lint error.

--- tests/test-proxy-connect.js
+++ tests/test-proxy-connect.js
@@ -66,7 +66,7 @@ tape('proxy', function(t) {
       'dont-send-to-proxy: ok',
       'accept: yo',
       'user-agent: just another foobar',
-      'host: google.com',
+      'host: google.com'
     ].join('\r\n'))
     t.equal(true, re.test(data))
     t.equal(called, true, 'the request must be made to the proxy server')
