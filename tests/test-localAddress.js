1f6fbd57b8e2be0071e9771846790dda137ebb9a
simov <simeonvelichkov@gmail.com>
2015-02-26 08:23:49 +0200

    Fix indentation and localAddress check in init

--- tests/test-localAddress.js
+++ tests/test-localAddress.js
@@ -1,49 +1,49 @@
 'use strict'
 var request = require('../index')
-    , tape = require('tape')
+  , tape = require('tape')
 
 tape('bind to invalid address', function (t) {
-    request.get({
-        uri: 'http://www.google.com',
-        localAddress: '1.2.3.4'
-    }, function (err, res) {
-        t.notEqual(err, null)
-        t.equal(err.message, 'bind EADDRNOTAVAIL')
-        t.equal(res, undefined)
-        t.end()
-    })
+  request.get({
+    uri: 'http://www.google.com',
+    localAddress: '1.2.3.4'
+  }, function (err, res) {
+    t.notEqual(err, null)
+    t.equal(err.message, 'bind EADDRNOTAVAIL')
+    t.equal(res, undefined)
+    t.end()
+  })
 })
 
 tape('bind to local address', function (t) {
-    request.get({
-        uri: 'http://www.google.com',
-        localAddress: '127.0.0.1'
-    }, function (err, res) {
-        t.notEqual(err, null)
-        t.equal(res, undefined)
-        t.end()
-    })
+  request.get({
+    uri: 'http://www.google.com',
+    localAddress: '127.0.0.1'
+  }, function (err, res) {
+    t.notEqual(err, null)
+    t.equal(res, undefined)
+    t.end()
+  })
 })
 
 tape('bind to local address on redirect', function (t) {
-    var os = require('os')
-    var localInterfaces = os.networkInterfaces()
-    var localIPS = []
-    Object.keys(localInterfaces).forEach(function (ifname) {
-        localInterfaces[ifname].forEach(function (iface) {
-            if (iface.family !== 'IPv4' || iface.internal !== false) {
-                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-                return
-            }
-                localIPS.push(iface.address)
-        })
+  var os = require('os')
+  var localInterfaces = os.networkInterfaces()
+  var localIPS = []
+  Object.keys(localInterfaces).forEach(function (ifname) {
+    localInterfaces[ifname].forEach(function (iface) {
+      if (iface.family !== 'IPv4' || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return
+      }
+      localIPS.push(iface.address)
     })
-    request.get({
-        uri: 'http://google.com', //redirects to 'http://google.com'
-        localAddress: localIPS[0]
-    }, function (err, res) {
-        t.equal(err, null)
-        t.equal(res.request.localAddress, localIPS[0])
-        t.end()
-    })
+  })
+  request.get({
+    uri: 'http://google.com', //redirects to 'http://google.com'
+    localAddress: localIPS[0]
+  }, function (err, res) {
+    t.equal(err, null)
+    t.equal(res.request.localAddress, localIPS[0])
+    t.end()
+  })
 })
