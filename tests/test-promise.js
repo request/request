45623b73979c1da46dee8f2da0f89a1585a50d13
simov <simeonvelichkov@gmail.com>
2015-03-31 12:38:09 +0300

    Add promise tests

---
+++ tests/test-promise.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var http = require('http')
+  , request = require('../index')
+  , tape = require('tape')
+  , Promise = require('bluebird')
+
+var s = http.createServer(function(req, res) {
+  res.writeHead(200, {})
+  res.end('ok')
+})
+
+tape('setup', function(t) {
+  s.listen(6767, function() {
+    t.end()
+  })
+})
+
+tape('promisify convenience method', function(t) {
+  var get = request.get
+  var p = Promise.promisify(get)
+  p('http://localhost:6767')
+    .then(function (results) {
+      var res = results[0]
+      t.equal(res.statusCode, 200)
+      t.end()
+    })
+})
+
+tape('promisify request function', function(t) {
+  var p = Promise.promisify(request)
+  p('http://localhost:6767')
+    .spread(function (res, body) {
+      t.equal(res.statusCode, 200)
+      t.end()
+    })
+})
+
+tape('promisify all methods', function(t) {
+  Promise.promisifyAll(request)
+  request.getAsync('http://localhost:6767')
+    .spread(function (res, body) {
+      t.equal(res.statusCode, 200)
+      t.end()
+    })
+})
+
+tape('cleanup', function(t) {
+  s.close(function() {
+    t.end()
+  })
+})
