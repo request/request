b54a7d786c511e2577f6b3c4354a5795976e8b25
mgorczyca <mgorczyca@users.noreply.github.com>
2015-05-13 08:43:43 -0700

    Test for forever-agent PR #29 - Enable socket reuse
    Added as requested by @simov.

--- tests/test-agent.js
+++ tests/test-agent.js
@@ -15,6 +15,35 @@ tape('setup', function(t) {
   })
 })
 
+tape('should have one persistent socket for two POST requests', function(t) {
+  var options = {
+    url: 'http://localhost:6767',
+    headers: { 'Connection':'keep-alive' },
+    method: 'POST',
+    body: 'name=abc',
+    forever: true
+   }
+
+  request(options, function (err, response, body) {
+    t.equal(err, null)
+    if (!err) {
+      request(options, function (err, response2, body) {
+        t.equal(err, null)
+        t.equal(response.socket, response2.socket)
+        response.socket.destroy()
+        response2.socket.destroy()
+        t.end()
+      })
+    }
+    else {
+        response.socket.destroy()
+        t.end()
+    }
+  })
+})
+
+
+
 tape('should work with forever agent', function(t) {
   var r = request.forever({maxSockets: 1})
 
