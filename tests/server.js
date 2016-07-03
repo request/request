d626daa59b5001b4d289ae5c0a90e23b24b61cfe
Emerson Borges <emersonbrg@gmail.com>
2016-07-03 19:14:08 -0300

    SNI test

--- tests/server.js
+++ tests/server.js
@@ -57,7 +57,7 @@ exports.createSSLServer = function(port, opts) {
   }
 
   for (i in options) {
-    if (i !== 'requestCert' && i !== 'rejectUnauthorized' && i !== 'ciphers') {
+    if (i !== 'requestCert' && i !== 'rejectUnauthorized' && i !== 'ciphers' && i !== 'SNICallback') {
       options[i] = fs.readFileSync(options[i])
     }
   }
