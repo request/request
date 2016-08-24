9f48b9f8c911571cf36f296f843c2d80746d2ee2
Scott Trinh <strinh@Scotts-MBP-2.shootproof.com>
2016-08-24 15:40:36 -0400

    Emit progress event on request write

--- request.js
+++ request.js
@@ -120,6 +120,7 @@ function Request (options) {
   if (options.method) {
     self.explicitMethod = true
   }
+  self.transferred = 0;
   self._qs = new Querystring(self)
   self._auth = new Auth(self)
   self._oauth = new OAuth(self)
@@ -1380,6 +1381,12 @@ Request.prototype.write = function () {
     self.start()
   }
   if (self.req) {
+    const contentLength = self.req._headers['content-length'];
+    self.emit('progress', {
+      lengthComputable: !!contentLength,
+      total: contentLength,
+      loaded: self.transferred += Buffer.byteLength(arguments[0])
+    });
     return self.req.write.apply(self.req, arguments)
   }
 }
