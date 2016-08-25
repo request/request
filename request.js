ac96a82342a31e2e65211261f73c384ff23a3202
Scott Trinh <strinh@Scotts-MBP-2.shootproof.com>
2016-08-25 16:07:08 -0400

    Fix linting errors and const->var

--- request.js
+++ request.js
@@ -120,7 +120,7 @@ function Request (options) {
   if (options.method) {
     self.explicitMethod = true
   }
-  self.transferred = 0;
+  self.transferred = 0
   self._qs = new Querystring(self)
   self._auth = new Auth(self)
   self._oauth = new OAuth(self)
@@ -1381,12 +1381,12 @@ Request.prototype.write = function () {
     self.start()
   }
   if (self.req) {
-    const contentLength = self.req._headers['content-length'];
+    var contentLength = self.req._headers['content-length']
     self.emit('progress', {
       lengthComputable: !!contentLength,
       total: contentLength,
       loaded: self.transferred += Buffer.byteLength(arguments[0])
-    });
+    })
     return self.req.write.apply(self.req, arguments)
   }
 }
