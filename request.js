0521207a9e6707c86c8b08197736800681b16ac4
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2016-11-21 15:45:46 +0100

    Enabled possibility of catching errors for piped readable streams

--- request.js
+++ request.js
@@ -516,6 +516,9 @@ Request.prototype.init = function (options) {
       }
     }
 
+    // Allow for catching stream errors in parent context.
+    src.on('error', function() {})
+
     // self.on('pipe', function () {
     //   console.error('You have already piped to this stream. Pipeing twice is likely to break the request.')
     // })
