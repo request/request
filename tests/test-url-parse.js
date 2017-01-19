21a993349d090d466534441a5cd07971d8bb75cf
SamvelRaja <samvel@getpostman.com>
2017-01-19 18:59:03 +0530

    Tests for already encoded url(+) and additional language characters

--- tests/test-url-parse.js
+++ tests/test-url-parse.js
@@ -235,6 +235,63 @@ tape('stringify - "a=b&&c=d"', function (t) {
   t.end()
 })
 
+tape('stringify - "email=foo+bar-xyz@gmail.com"', function (t) {
+  var parsed = [
+        { key: 'email', value: 'foo+bar-xyz@gmail.com' }
+  ]
+  t.equal(url.stringify(parsed), 'email=foo+bar-xyz@gmail.com')
+  t.end()
+})
+
+tape('stringify - "email=foo%2Bbar-xyz@gmail.com"', function (t) {
+  var parsed = [
+        { key: 'email', value: 'foo%2Bbar-xyz@gmail.com' }
+  ]
+  t.equal(url.stringify(parsed), 'email=foo%2Bbar-xyz@gmail.com')
+  t.end()
+})
+
+tape('stringify - "a[0]=foo&a[1]=bar"', function (t) {
+  var parsed = [
+        { key: 'a[0]', value: 'foo' },
+        { key: 'a[1]', value: 'bar' }
+  ]
+  t.equal(url.stringify(parsed), 'a[0]=foo&a[1]=bar')
+  t.end()
+})
+
+tape('stringify Russian - "a=Привет Почтальон"', function (t) {
+  var parsed = [
+        { key: 'a', value: 'Привет Почтальон' },
+  ]
+  t.equal(url.stringify(parsed), 'a=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD')
+  t.end()
+})
+
+tape('stringify Chinese- "a=你好"', function (t) {
+  var parsed = [
+        { key: 'a', value: '你好' },
+  ]
+  t.equal(url.stringify(parsed), 'a=%E4%BD%A0%E5%A5%BD')
+  t.end()
+})
+
+tape('stringify Japanese- "a=ハローポストマン"', function (t) {
+  var parsed = [
+        { key: 'a', value: 'ハローポストマン' },
+  ]
+  t.equal(url.stringify(parsed), 'a=%E3%83%8F%E3%83%AD%E3%83%BC%E3%83%9D%E3%82%B9%E3%83%88%E3%83%9E%E3%83%B3')
+  t.end()
+})
+
+tape('stringify Partial Russian - "a=Hello Почтальон"', function (t) {
+  var parsed = [
+        { key: 'a', value: 'Hello Почтальон' },
+  ]
+  t.equal(url.stringify(parsed), 'a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD')
+  t.end()
+})
+
 tape('url parse - "http://httpbin.org/get?z=b,c"', function (t) {
   var parsed = url('http://httpbin.org/get?z=b,c')
 
