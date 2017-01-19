7106be9f8e5e9e56d7eacb20be26ad7e5f60e5ce
SamvelRaja <samvel@getpostman.com>
2017-01-19 19:28:00 +0530

    Added tests for russian pre-encoded params

--- tests/test-url-parse.js
+++ tests/test-url-parse.js
@@ -243,7 +243,7 @@ tape('stringify - "email=foo+bar-xyz@gmail.com"', function (t) {
   t.end()
 })
 
-tape('stringify - "email=foo%2Bbar-xyz@gmail.com"', function (t) {
+tape('stringify pre encoded text(+) - "email=foo%2Bbar-xyz@gmail.com"', function (t) {
   var parsed = [
         { key: 'email', value: 'foo%2Bbar-xyz@gmail.com' }
   ]
@@ -287,6 +287,14 @@ tape('stringify Japanese- "a=ハローポストマン"', function (t) {
 tape('stringify Partial Russian - "a=Hello Почтальон"', function (t) {
   var parsed = [
         { key: 'a', value: 'Hello Почтальон' },
+  ]
+  t.equal(url.stringify(parsed), 'a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD')
+  t.end()
+})
+
+tape('stringify pre encoded russian text - a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD', function (t) {
+  var parsed = [
+        { key: 'a', value: 'Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD' }
   ]
   t.equal(url.stringify(parsed), 'a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD')
   t.end()
