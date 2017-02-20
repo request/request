b2605877b139b3827c8cfac49785edc97f4f7630
Fargier Sylvain <fargier.sylvain@free.fr>
2017-02-20 14:36:56 +0100

    Add proxy timeout support
      - timeout value can be set in request.proxy.timeout or request.timeout

---
+++ tests/test-proxy-timeout.js
@@ -0,0 +1,54 @@
+'use strict'
+
+var request = require('../index')
+  , url = require('url')
+  , tape = require('tape')
+
+var called = false
+  , proxiedHost = 'google.com'
+  , data = ''
+
+var s = require('net').createServer(function(sock) {
+  called = true
+  sock.once('data', function (c) {
+    data += c
+  })
+})
+
+tape('setup', function(t) {
+  s.listen(0, function() {
+    s.url = 'http://localhost:' + this.address().port
+    t.end()
+  })
+})
+
+tape('proxy opts.proxy.timeout', function(t) {
+  request({
+    tunnel: true,
+    url: 'http://' + proxiedHost,
+    proxy: { port: s.address().port, hostname: 'localhost', timeout: 100 }
+  }, function(err, res, body) {
+    t.notEqual(err, null)
+    t.ok(err.code === 'ECONNRESET')
+    t.end()
+  })
+})
+
+tape('proxy opts.timeout', function(t) {
+  request({
+    tunnel: true,
+    url: 'http://' + proxiedHost,
+    proxy: s.url,
+    timeout: 100
+  }, function(err, res, body) {
+    t.notEqual(err, null)
+    t.ok(err.code === 'ECONNRESET')
+    t.end()
+  })
+})
+
+tape('cleanup', function(t) {
+  s.close(function() {
+    t.end()
+  })
+})
