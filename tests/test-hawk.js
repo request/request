0825a0ae5bb6f0333997de29fced6d8c0a6cb8a5
Eran Hammer <eran@hammer.io>
2018-05-16 04:02:46 -0700

    Replace hawk dependency with local implementation

--- tests/test-hawk.js
+++ tests/test-hawk.js
@@ -2,27 +2,16 @@
 
 var http = require('http')
 var request = require('../index')
-var hawk = require('hawk')
+var hawk = require('../lib/hawk')
 var tape = require('tape')
 var assert = require('assert')
 
 var server = http.createServer(function (req, res) {
-  var getCred = function (id, callback) {
-    assert.equal(id, 'dh37fgj492je')
-    var credentials = {
-      key: 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn',
-      algorithm: 'sha256',
-      user: 'Steve'
-    }
-    return callback(null, credentials)
-  }
-
-  hawk.server.authenticate(req, getCred, {}, function (err, credentials, attributes) {
-    res.writeHead(err ? 401 : 200, {
-      'Content-Type': 'text/plain'
-    })
-    res.end(err ? 'Shoosh!' : 'Hello ' + credentials.user)
+  var user = authenticate(req)
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
   })
+  res.end('Hello ' + user)
 })
 
 tape('setup', function (t) {
@@ -53,3 +42,37 @@ tape('cleanup', function (t) {
     t.end()
   })
 })
+
+function authenticate (req) {
+  var headerParts = req.headers.authorization.match(/^(\w+)(?:\s+(.*))?$/)
+  assert.equal(headerParts[1], 'Hawk')
+  var attributes = {}
+  headerParts[2].replace(/(\w+)="([^"\\]*)"\s*(?:,\s*|$)/g, function ($0, $1, $2) { attributes[$1] = $2 })
+  var hostParts = req.headers.host.split(':')
+
+  const artifacts = {
+    method: req.method,
+    host: hostParts[0],
+    port: (hostParts[1] ? hostParts[1] : (req.connection && req.connection.encrypted ? 443 : 80)),
+    resource: req.url,
+    ts: attributes.ts,
+    nonce: attributes.nonce,
+    hash: attributes.hash,
+    ext: attributes.ext,
+    app: attributes.app,
+    dlg: attributes.dlg,
+    mac: attributes.mac,
+    id: attributes.id
+  }
+
+  assert.equal(attributes.id, 'dh37fgj492je')
+  var credentials = {
+    key: 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn',
+    algorithm: 'sha256',
+    user: 'Steve'
+  }
+
+  const mac = hawk.calculateMac(credentials, artifacts)
+  assert.equal(mac, attributes.mac)
+  return credentials.user
+}
