1befd90b4b5ea11149b4f1d23c2c6773196da863
Francis Gulotta <wizard@roborooter.com>
2018-11-22 10:33:09 -0500

    fix: SSL Certs (#3062)
    - Small readme talking about the issue
    - Generate all certs for 10 years
    - one command to update all of them

--- tests/ssl/ca/gen-server.sh
+++ tests/ssl/ca/gen-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -ex
 # fixes:
 # Error: error:140AB18F:SSL routines:SSL_CTX_use_certificate:ee key too small
 # on Node > v10
@@ -8,6 +8,11 @@ openssl genrsa 4096 > server.key
 
 openssl req -new -nodes -sha256 -key server.key -config server.cnf -out server.csr
 
-openssl x509 -req -sha256 -in server.csr -CA ca.crt -CAkey ca.key -out server.crt
-
-# password: password
+openssl x509 -req \
+  -sha256 \
+  -in server.csr \
+  -CA ca.crt \
+  -CAkey ca.key \
+  -out server.crt \
+  -passin 'pass:password' \
+  -days 3650
