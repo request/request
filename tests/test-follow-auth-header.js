57f10f28c66f0916522d12e4b956916fa29341ef
Udit Vasu <udit.vasu@getpostman.com>
2019-01-20 22:31:39 +0530

    Add tests for all redirect status code + authorization header

--- tests/test-follow-auth-header.js
+++ tests/test-follow-auth-header.js
@@ -6,86 +6,95 @@ var destroyable = require('server-destroy')
 var server = require('./server')
 var request = require('../index')
 
-var s = server.createServer()
+function runTest (t, statusCode, followAuthorizationHeader) {
+  var s = server.createServer()
+  var redirects = 0
+  var authHeader = 'Basic aGVsbG86d29ybGQ='
 
-destroyable(s)
+  destroyable(s)
 
-s.on('/', function (req, res) {
-  if (req.headers.host === `${s.redirectHost}:${s.port}`) {
-    res.writeHead(302, { location: `http://${s.respondHost}:${s.port}/` })
-    res.end()
-  } else if (req.headers.host === `${s.respondHost}:${s.port}`) {
-    res.writeHead(200)
-    res.end('ok')
-  } else {
-    res.writeHead(400)
-    res.end('unknown host')
-  }
-})
+  s.on('/', function (req, res) {
+    if (req.headers.host === `${s.redirectHost}:${s.port}`) {
+      res.writeHead(statusCode || 302, {
+        location: `http://${s.respondHost}:${s.port}/`
+      })
+      res.end()
+    } else if (req.headers.host === `${s.respondHost}:${s.port}`) {
+      res.writeHead(200)
+      res.end('ok')
+    } else {
+      res.writeHead(400)
+      res.end('unknown host')
+    }
+  })
 
-tape('setup', function (t) {
   s.listen(0, function () {
     s.redirectHost = 'test1.local.omg' // resolves to 127.0.0.1
     s.respondHost = 'test2.local.omg' // resolves to 127.0.0.1
-    t.end()
+
+    request({
+      url: `http://${s.redirectHost}:${s.port}`,
+      headers: {
+        authorization: authHeader
+      },
+      followAllRedirects: true,
+      followRedirect: true,
+      followAuthorizationHeader: followAuthorizationHeader,
+      lookup: function (hostname, options, callback) {
+        callback(null, '127.0.0.1', 4) // All hosts will resolve to 127.0.0.1
+      }
+    }, function (err, res, body) {
+      t.equal(err, null)
+      t.equal(redirects, 1)
+      t.equal(body.toString(), 'ok')
+      t.equal(res.request.headers.authorization, followAuthorizationHeader ? authHeader : undefined)
+      s.destroy(function () {
+        t.end()
+      })
+    }).on('redirect', function () {
+      redirects++
+      t.equal(this.response.statusCode, statusCode)
+      t.equal(this.uri.href, `http://${s.respondHost}:${s.port}/`)
+    })
   })
+}
+
+tape('301 redirect', function (t) {
+  runTest(t, 301)
 })
 
-tape('drop authorization header when redirects to different host', function (t) {
-  var redirects = 0
-  var authHeader = 'Basic aGVsbG86d29ybGQ='
-
-  request({
-    url: `http://${s.redirectHost}:${s.port}/`,
-    headers: {
-      authorization: authHeader
-    },
-    followAllRedirects: true,
-    followRedirect: true,
-    lookup: function (hostname, options, callback) {
-      callback(null, '127.0.0.1', 4) // All hosts will resolve to 127.0.0.1
-    }
-  }, function (err, res, body) {
-    t.equal(err, null)
-    t.equal(redirects, 1)
-    t.equal(body.toString(), 'ok')
-    t.equal(res.request.headers.authorization, undefined)
-    t.end()
-  }).on('redirect', function () {
-    redirects++
-    t.equal(this.uri.href, `http://${s.respondHost}:${s.port}/`)
-  })
+tape('301 redirect + followAuthorizationHeader', function (t) {
+  runTest(t, 301, true)
 })
 
-tape('retain authorization header on redirects when using followAuthorizationHeader', function (t) {
-  var redirects = 0
-  var authHeader = 'Basic aGVsbG86d29ybGQ='
-
-  request({
-    url: `http://${s.redirectHost}:${s.port}/`,
-    headers: {
-      authorization: authHeader
-    },
-    followAllRedirects: true,
-    followRedirect: true,
-    followAuthorizationHeader: true,
-    lookup: function (hostname, options, callback) {
-      callback(null, '127.0.0.1', 4) // All hosts will resolve to 127.0.0.1
-    }
-  }, function (err, res, body) {
-    t.equal(err, null)
-    t.equal(redirects, 1)
-    t.equal(body.toString(), 'ok')
-    t.equal(res.request.headers.authorization, authHeader)
-    t.end()
-  }).on('redirect', function () {
-    redirects++
-    t.equal(this.uri.href, `http://${s.respondHost}:${s.port}/`)
-  })
+tape('302 redirect', function (t) {
+  runTest(t, 302)
 })
 
-tape('cleanup', function (t) {
-  s.destroy(function () {
-    t.end()
-  })
+tape('302 redirect + followAuthorizationHeader', function (t) {
+  runTest(t, 302, true)
+})
+
+tape('303 redirect', function (t) {
+  runTest(t, 303)
+})
+
+tape('303 redirect + followAuthorizationHeader', function (t) {
+  runTest(t, 303, true)
+})
+
+tape('307 redirect', function (t) {
+  runTest(t, 307)
+})
+
+tape('307 redirect + followAuthorizationHeader', function (t) {
+  runTest(t, 307, true)
+})
+
+tape('308 redirect', function (t) {
+  runTest(t, 308)
+})
+
+tape('308 redirect + followAuthorizationHeader', function (t) {
+  runTest(t, 308, true)
 })
