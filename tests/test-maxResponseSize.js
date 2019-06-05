73f40919606a53800b4f8ae66eb83dc31c93268a
Premang Vikani <premangvikani@gmail.com>
2019-06-05 14:47:19 +0530

    Used local server in test instead of postman-echo

--- tests/test-maxResponseSize.js
+++ tests/test-maxResponseSize.js
@@ -1,12 +1,54 @@
 var request = require('../index')
+var http = require('http')
+var zlib = require('zlib')
 var tape = require('tape')
+var url = require('url')
 
-var echoDomain = 'https://postman-echo.com'
+var CHAR = 'X'
+
+// request path to this server should be of the form '/<bytes>?gzip=[true/false]'
+// response from the server will have size of <bytes> from request path
+var server = http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url, {parseQueryString: true})
+  var bytes = parseInt(parsedUrl.pathname.substring(1)) || 0
+  var gzip = parsedUrl.query.gzip
+  var data = Buffer.from(CHAR.repeat(bytes))
+
+  res.setHeader('Content-Type', 'text/plain')
+
+  if (gzip === 'true') {
+    zlib.gzip(data, function (err, compressedData) {
+      if (err) {
+        res.writeHead(500)
+        res.end()
+      }
+
+      res.setHeader('Content-Encoding', 'gzip')
+      res.setHeader('Content-Length', compressedData.length)
+      res.writeHead(200)
+      res.write(compressedData)
+      res.end()
+    })
+  } else {
+    res.setHeader('Content-Length', data.length)
+    res.writeHead(200)
+    res.write(data)
+    res.end()
+  }
+})
+
+tape('setup', function (t) {
+  server.listen(0, function () {
+    server.port = this.address().port
+    server.url = 'http://localhost:' + server.port
+    t.end()
+  })
+})
 
 tape('response < maxResponseSize', function (t) {
   var options = {
     method: 'GET',
-    uri: echoDomain + '/bytes/50',
+    uri: server.url + '/50',
     maxResponseSize: 100
   }
 
@@ -21,7 +63,7 @@ tape('response < maxResponseSize', function (t) {
 tape('response = maxResponseSize', function (t) {
   var options = {
     method: 'GET',
-    uri: echoDomain + '/bytes/100',
+    uri: server.url + '/100',
     maxResponseSize: 100
   }
 
@@ -36,7 +78,7 @@ tape('response = maxResponseSize', function (t) {
 tape('response > maxResponseSize', function (t) {
   var options = {
     method: 'GET',
-    uri: echoDomain + '/bytes/200',
+    uri: server.url + '/200',
     maxResponseSize: 100
   }
 
@@ -52,12 +94,9 @@ tape('response > maxResponseSize', function (t) {
 tape('extracted gzip response > maxResponseSize but content-length < maxResponseSize', function (t) {
   var options = {
     method: 'GET',
-    uri: echoDomain + '/bytes/500', // for random 500 bytes gzip response, content-length will be around 350
+    uri: server.url + '/500?gzip=true', // for 500 bytes gzip response, content-length will be around 30
     maxResponseSize: 490,
-    gzip: true,
-    headers: {
-      'Accept-Encoding': 'gzip'
-    }
+    gzip: true
   }
 
   request(options, function (err, res, body) {
@@ -72,18 +111,21 @@ tape('extracted gzip response > maxResponseSize but content-length < maxResponse
 tape('extracted gzip response < maxResponseSize', function (t) {
   var options = {
     method: 'GET',
-    uri: echoDomain + '/bytes/100',
+    uri: server.url + '/100?gzip=true',
     maxResponseSize: 200,
-    gzip: true,
-    headers: {
-      'Accept-Encoding': 'gzip'
-    }
+    gzip: true
   }
 
   request(options, function (err, res, body) {
     t.equal(err, null)
     t.ok(body, 'Should receive body')
     t.ok(body.length < options.maxResponseSize)
+    t.end()
+  })
+})
+
+tape('cleanup', function (t) {
+  server.close(function () {
     t.end()
   })
 })
