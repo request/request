b4003e088d463d278d250e35f5114140b81c7d58
Utkarsh Maheshwari <coditva@gmail.com>
2019-06-28 16:02:06 +0530

    Add test for content-length with content-type

--- tests/test-form-data-boundary.js
+++ tests/test-form-data-boundary.js
@@ -100,6 +100,30 @@ tape('custom boundary within quotes', function (t) {
   })
 })
 
+tape('content-length without content-type', function (t) {
+  request.post({
+    url: server.url,
+    headers: {
+      'content-length': '171'
+    },
+    formData: {
+      formKey: 'formValue'
+    }
+  }, function (err, res, body) {
+    var req = JSON.parse(body)
+    var boundary
+    t.equal(err, null)
+    t.equal(res.statusCode, 200)
+    t.notEqual(req.headers['content-type'], null)
+    boundary = req.headers['content-type'].split('boundary=')[1]
+    t.equal(req.headers['content-type'], 'multipart/form-data; boundary=' + boundary)
+    t.equal(req.headers['content-length'], '171')
+    t.ok(req.body.indexOf('name="formKey"') !== -1)
+    t.ok(req.body.indexOf('formValue') !== -1)
+    t.end()
+  })
+})
+
 tape('custom boundary with content-length', function (t) {
   var boundary = 'X-FORM-DATA-BOUNDARY'
   request.post({
@@ -126,7 +150,7 @@ tape('custom boundary with content-length', function (t) {
 })
 
 tape('custom boundary and charset', function (t) {
-  var boundary = '----WebKitFormBoundary7MA4YWxkTrZu0gW'
+  var boundary = 'X-FORM-DATA-BOUNDARY'
   request.post({
     url: server.url,
     headers: {
