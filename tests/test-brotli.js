eee7116e59a40c7651a4ac1d60095cf2b52aa447
Utkarsh Maheshwari <coditva@gmail.com>
2020-01-03 17:34:36 +0530

    Add tests for brotli

--- tests/test-brotli.js
+++ tests/test-brotli.js
@@ -2,68 +2,53 @@
 
 var request = require('../index')
 var http = require('http')
-var zlib = require('zlib')
-var assert = require('assert')
 var bufferEqual = require('buffer-equal')
 var tape = require('tape')
+var Buffer = require('safe-buffer').Buffer
+var brotliCompress = require('brotli/compress')
 
-var testContent = 'Compressible response content.\n'
+var testContent = 'compressible response content.\n'
+var testContentBrotli = Buffer.from(brotliCompress(Buffer.from(testContent)))
 var testContentBig
-var testContentBigGzip
-var testContentGzip
+var testContentBigBrotli
 
 var server = http.createServer(function (req, res) {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
 
   if (req.method === 'HEAD') {
-    res.setHeader('Content-Encoding', 'gzip')
+    res.setHeader('Content-Encoding', 'br')
     res.end()
     return
   }
   if (req.headers.code) {
     res.writeHead(req.headers.code, {
-      'Content-Encoding': 'gzip',
+      'Content-Encoding': 'br',
       code: req.headers.code
     })
     res.end()
     return
   }
 
-  if (/\bgzip\b/i.test(req.headers['accept-encoding'])) {
-    res.setHeader('Content-Encoding', 'gzip')
+  if (/\bbr\b/i.test(req.headers['accept-encoding'])) {
+    res.setHeader('Content-Encoding', 'br')
     if (req.url === '/error') {
-      // send plaintext instead of gzip (should cause an error for the client)
+      // send plaintext instead of br (should cause an error for the client)
       res.end(testContent)
     } else if (req.url === '/chunks') {
       res.writeHead(200)
-      res.write(testContentBigGzip.slice(0, 4096))
-      setTimeout(function () { res.end(testContentBigGzip.slice(4096)) }, 10)
-    } else if (req.url === '/just-slightly-truncated') {
-      zlib.gzip(testContent, function (err, data) {
-        assert.equal(err, null)
-        // truncate the CRC checksum and size check at the end of the stream
-        res.end(data.slice(0, data.length - 8))
-      })
+      res.write(Buffer.from(testContentBigBrotli.slice(0, 4096)))
+      setTimeout(function () { res.end(Buffer.from(testContentBigBrotli.slice(4096))) }, 10)
     } else {
-      zlib.gzip(testContent, function (err, data) {
-        assert.equal(err, null)
-        res.end(data)
-      })
+      res.end(testContentBrotli)
     }
-  } else if (/\bdeflate\b/i.test(req.headers['accept-encoding'])) {
-    res.setHeader('Content-Encoding', 'deflate')
-    zlib.deflate(testContent, function (err, data) {
-      assert.equal(err, null)
-      res.end(data)
-    })
   } else {
     res.end(testContent)
   }
 })
 
 tape('setup', function (t) {
-  // Need big compressed content to be large enough to chunk into gzip blocks.
+  // Need big compressed content to be large enough to chunk into brotli blocks.
   // Want it to be deterministic to ensure test is reliable.
   // Generate pseudo-random printable ASCII characters using MINSTD
   var a = 48271
@@ -76,44 +61,26 @@ tape('setup', function (t) {
     testContentBig[i] = (x % 95) + 32
   }
 
-  zlib.gzip(testContent, function (err, data) {
-    t.equal(err, null)
-    testContentGzip = data
+  testContentBigBrotli = brotliCompress(Buffer.from(testContentBig))
 
-    zlib.gzip(testContentBig, function (err, data2) {
-      t.equal(err, null)
-      testContentBigGzip = data2
-
-      server.listen(0, function () {
-        server.url = 'http://localhost:' + this.address().port
-        t.end()
-      })
-    })
+  server.listen(0, function () {
+    server.url = 'http://localhost:' + this.address().port
+    t.end()
   })
 })
 
-tape('transparently supports gzip decoding to callbacks', function (t) {
-  var options = { url: server.url + '/foo', gzip: true }
+tape('transparently supports brotli decoding to callbacks', function (t) {
+  var options = { url: server.url + '/foo', brotli: true }
   request.get(options, function (err, res, body) {
     t.equal(err, null)
-    t.equal(res.headers['content-encoding'], 'gzip')
+    t.equal(res.headers['content-encoding'], 'br')
     t.equal(body, testContent)
     t.end()
   })
 })
 
-tape('supports slightly invalid gzip content', function (t) {
-  var options = { url: server.url + '/just-slightly-truncated', gzip: true }
-  request.get(options, function (err, res, body) {
-    t.equal(err, null)
-    t.equal(res.headers['content-encoding'], 'gzip')
-    t.equal(body, testContent)
-    t.end()
-  })
-})
-
-tape('transparently supports gzip decoding to pipes', function (t) {
-  var options = { url: server.url + '/foo', gzip: true }
+tape('transparently supports brotli decoding to pipes', function (t) {
+  var options = { url: server.url + '/foo', brotli: true }
   var chunks = []
   request.get(options)
     .on('data', function (chunk) {
@@ -128,12 +95,12 @@ tape('transparently supports gzip decoding to pipes', function (t) {
     })
 })
 
-tape('does not request gzip if user specifies Accepted-Encodings', function (t) {
+tape('does not request brotli if user specifies Accepted-Encodings', function (t) {
   var headers = { 'Accept-Encoding': null }
   var options = {
     url: server.url + '/foo',
     headers: headers,
-    gzip: true
+    brotli: true
   }
   request.get(options, function (err, res, body) {
     t.equal(err, null)
@@ -143,34 +110,34 @@ tape('does not request gzip if user specifies Accepted-Encodings', function (t) 
   })
 })
 
-tape('does not decode gzip encoding when "brotli" option is set', function (t) {
-  var headers = { 'Accept-Encoding': 'gzip' }
-  var options = { url: server.url + '/foo', headers: headers, brotli: true }
-  request.get(options, function (err, res, body) {
-    t.equal(err, null)
-    t.equal(res.headers['content-encoding'], 'gzip')
-    t.equal(body, testContentGzip.toString())
-    t.end()
-  })
-})
-
 tape('does not decode user-requested encoding by default', function (t) {
-  var headers = { 'Accept-Encoding': 'gzip' }
+  var headers = { 'Accept-Encoding': 'br' }
   var options = { url: server.url + '/foo', headers: headers }
   request.get(options, function (err, res, body) {
     t.equal(err, null)
-    t.equal(res.headers['content-encoding'], 'gzip')
-    t.equal(body, testContentGzip.toString())
+    t.equal(res.headers['content-encoding'], 'br')
+    t.equal(body, testContentBrotli.toString())
     t.end()
   })
 })
 
-tape('supports character encoding with gzip encoding', function (t) {
-  var headers = { 'Accept-Encoding': 'gzip' }
+tape('does not decode brotli encoding when "gzip" option is set', function (t) {
+  var headers = { 'Accept-Encoding': 'br' }
+  var options = { url: server.url + '/foo', headers: headers, gzip: true }
+  request.get(options, function (err, res, body) {
+    t.equal(err, null)
+    t.equal(res.headers['content-encoding'], 'br')
+    t.equal(body, testContentBrotli.toString())
+    t.end()
+  })
+})
+
+tape('supports character encoding with brotli encoding', function (t) {
+  var headers = { 'Accept-Encoding': 'br' }
   var options = {
     url: server.url + '/foo',
     headers: headers,
-    gzip: true,
+    brotli: true,
     encoding: 'utf8'
   }
   var strings = []
@@ -188,18 +155,18 @@ tape('supports character encoding with gzip encoding', function (t) {
     })
 })
 
-tape('transparently supports gzip error to callbacks', function (t) {
-  var options = { url: server.url + '/error', gzip: true }
+tape('transparently supports brotli error to callbacks', function (t) {
+  var options = { url: server.url + '/error', brotli: true }
   request.get(options, function (err, res, body) {
-    t.equal(err.code, 'Z_DATA_ERROR')
+    t.equal(err.code, 'ERR_PADDING_1')
     t.equal(res, undefined)
     t.equal(body, undefined)
     t.end()
   })
 })
 
-tape('transparently supports gzip error to pipes', function (t) {
-  var options = { url: server.url + '/error', gzip: true }
+tape('transparently supports brotli error to pipes', function (t) {
+  var options = { url: server.url + '/error', brotli: true }
   request.get(options)
     .on('data', function (chunk) {
       t.fail('Should not receive data event')
@@ -208,15 +175,15 @@ tape('transparently supports gzip error to pipes', function (t) {
       t.fail('Should not receive end event')
     })
     .on('error', function (err) {
-      t.equal(err.code, 'Z_DATA_ERROR')
+      t.equal(err.code, 'ERR_PADDING_1')
       t.end()
     })
 })
 
-tape('pause when streaming from a gzip request object', function (t) {
+tape('pause when streaming from a brotli request object', function (t) {
   var chunks = []
   var paused = false
-  var options = { url: server.url + '/chunks', gzip: true }
+  var options = { url: server.url + '/chunks', brotli: true }
   request.get(options)
     .on('data', function (chunk) {
       var self = this
@@ -240,9 +207,9 @@ tape('pause when streaming from a gzip request object', function (t) {
     })
 })
 
-tape('pause before streaming from a gzip request object', function (t) {
+tape('pause before streaming from a brotli request object', function (t) {
   var paused = true
-  var options = { url: server.url + '/foo', gzip: true }
+  var options = { url: server.url + '/foo', brotli: true }
   var r = request.get(options)
   r.pause()
   r.on('data', function (data) {
@@ -257,19 +224,8 @@ tape('pause before streaming from a gzip request object', function (t) {
   }, 100)
 })
 
-tape('transparently supports deflate decoding to callbacks', function (t) {
-  var options = { url: server.url + '/foo', gzip: true, headers: { 'Accept-Encoding': 'deflate' } }
-
-  request.get(options, function (err, res, body) {
-    t.equal(err, null)
-    t.equal(res.headers['content-encoding'], 'deflate')
-    t.equal(body, testContent)
-    t.end()
-  })
-})
-
 tape('do not try to pipe HEAD request responses', function (t) {
-  var options = { method: 'HEAD', url: server.url + '/foo', gzip: true }
+  var options = { method: 'HEAD', url: server.url + '/foo', brotli: true }
 
   request(options, function (err, res, body) {
     t.equal(err, null)
@@ -279,7 +235,7 @@ tape('do not try to pipe HEAD request responses', function (t) {
 })
 
 tape('do not try to pipe responses with no body', function (t) {
-  var options = { url: server.url + '/foo', gzip: true }
+  var options = { url: server.url + '/foo', brotli: true }
 
   // skip 105 on Node >= v10
   var statusCodes = process.version.split('.')[0].slice(1) >= 10
