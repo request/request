c25753a0da550e6d236d56228e6f772ee78e7d67
Udit Vasu <udit@pm.me>
2020-10-05 23:46:50 +0530

    Test: update form-data utf filename

--- tests/test-form-data-utf-filename.js
+++ tests/test-form-data-utf-filename.js
@@ -50,8 +50,8 @@ tape('with empty filename', function (t) {
     t.equal(err, null)
     t.equal(res.statusCode, 200)
     t.ok(req.body.indexOf('filename="マイページ情報.jpg"') !== -1)
-    t.ok(req.body.indexOf('filename*="UTF-8\'\'' +
-      '%E3%83%9E%E3%82%A4%E3%83%98%E3%82%9A%E3%83%BC%E3%82%B7%E3%82%99%E6%83%85%E5%A0%B1.jpg"') !== -1)
+    t.ok(req.body.indexOf('filename*=UTF-8\'\'' +
+      '%E3%83%9E%E3%82%A4%E3%83%98%E3%82%9A%E3%83%BC%E3%82%B7%E3%82%99%E6%83%85%E5%A0%B1.jpg') !== -1)
     t.end()
   })
 })
