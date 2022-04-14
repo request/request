32b90a848e6279dea6ddf1660ba1b70f81ed7d69
Antoine Coutellier <coutellier.antoine@gmail.com>
2022-04-14 14:53:41 +0200

    refactor: use the built-in performance module [sc-00] (#1)

--- request.js
+++ request.js
@@ -27,7 +27,7 @@ var hawk = require('./lib/hawk')
 var Multipart = require('./lib/multipart').Multipart
 var Redirect = require('./lib/redirect').Redirect
 var Tunnel = require('./lib/tunnel').Tunnel
-var now = require('performance-now')
+var { now } = require('perf_hooks').performance
 var Buffer = require('safe-buffer').Buffer
 
 var safeStringify = helpers.safeStringify
