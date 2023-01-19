9510d07c49f50285aa92ba8e30873da43f3333cc
Udit Vasu <udit@pm.me>
2023-01-19 21:09:19 +0530

    Update certs

--- tests/ssl/ca/gen-all-certs.sh
+++ tests/ssl/ca/gen-all-certs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -ex
 
+# Create a certificate authority
+openssl req -new -x509 -days 9999 -config ca.cnf -keyout ca.key -out ca.crt
+
 ./gen-server.sh
 ./gen-client.sh
 ./gen-localhost.sh
